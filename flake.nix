0cbe5d94eac20a055aec1873343aa35d00b3266e
nikelear <nike.lear@gmail.com>
2024-05-20 15:05:34 +0900

    add none

--- flake.nix
+++ flake.nix
@@ -68,6 +68,19 @@
         ];
       };
 
+      none = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./home_none.nix
+        ];
+      };
+
     };
 
   };
