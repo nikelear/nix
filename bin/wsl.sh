9e2a88538ba0e3db422757c11c72485d02e5fff3
nikelear <nike.lear@gmail.com>
2024-07-11 16:10:45 +0900

    fix wsl.sh

--- bin/wsl.sh
+++ bin/wsl.sh
@@ -1,28 +1,26 @@
 # for wsl
-if [ -d /mnt/c/windows ]; then
+tgt=$HOME/.local/bin
+mkdir -p tgt
 
-  tgt=$HOME/.local/bin
-  mkdir -p tgt
-
-  # link cmd
-  if [ ! -L $tgt/cmd ]; then
-    ln -s "/mnt/c/windows/system32/cmd.exe" $tgt/cmd
-  fi
-  
-  # link firefox
-  ffx="/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
-  if [ ! -L $tgt/firefox ]; then
-    if [ -f "$ffx" ]; then
-      ln -s "$ffx" $tgt/firefox
-    fi
-  fi
-
-  # link vscode
-  if [ ! -L $tgt/code ]; then
-    userprofile=$(cd /mnt/c && cmd /C echo %USERPROFILE% | tr -d '\r')
-    fd=$(echo "$userprofile" | awk -F'\\' '{print $NF}')
-    code="/mnt/c/Users/$fd/AppData/Local/Programs/Microsoft VS Code/bin/code"
-    ln -s "$code" $tgt/code
-  fi
-  
+# link cmd
+if [ ! -L $tgt/cmd ]; then
+  ln -s "/mnt/c/windows/system32/cmd.exe" $tgt/cmd
 fi
+
+# link firefox
+ffx="/mnt/c/Program Files/Mozilla Firefox/firefox.exe"
+if [ ! -L $tgt/firefox ]; then
+  if [ -f "$ffx" ]; then
+    ln -s "$ffx" $tgt/firefox
+  fi
+fi
+
+# link vscode
+if [ ! -L $tgt/code ]; then
+  userprofile=$(cd /mnt/c && cmd /C echo %USERPROFILE% | tr -d '\r')
+  fd=$(echo "$userprofile" | awk -F'\\' '{print $NF}')
+  code="/mnt/c/Users/$fd/AppData/Local/Programs/Microsoft VS Code/bin/code"
+  ln -s "$code" $tgt/code
+fi
+  
+sudo git config --system credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
