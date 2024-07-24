afa5340f4e864522b6d9ae20a16deb9426dedb66
nikelear <nike.lear@gmail.com>
2024-07-24 23:03:23 +0900

    add flake template

---
+++ flakes/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "pure";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, fenix }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+    devShells.x86_64-linux = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          python311
+        ];
+        shellHook = ''
+          exec $SHELL -l
+        '';
+      };
+    };
+  };
+}
