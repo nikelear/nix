e349e524e9142a9bcab4b987b36cbdc29cb51b67
nikelear <nike.lear@gmail.com>
2024-08-02 10:22:06 +0900

    add python flake

---
+++ flakes/python/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A poetry2nix project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+        pythonpkg = pkgs.python312;
+      in
+      {
+        packages = {
+          myapp = mkPoetryApplication { 
+            projectDir = self;
+            python = pythonpkg;
+          };
+          default = self.packages.${system}.myapp;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.myapp ];
+          packages = [ pkgs.poetry pythonpkg ];
+          shellHook = ''
+            python --version
+          '';
+        };
+      });
+}
