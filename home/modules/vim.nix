225e2e834588d723be219b210b22bf5cee073712
nikelear <nike.lear@gmail.com>
2024-08-05 17:01:19 +0900

    nvim settings now

--- home/modules/vim.nix
+++ home/modules/vim.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs; [
+    clang-tools
+  ];
   programs = {
     vim = {
       enable = true;
@@ -10,6 +13,44 @@
     };
     neovim = {
       enable = true;
+      plugins = with pkgs.vimPlugins; [
+        yankring
+        vim-nix
+      ];
+      extraConfig = builtins.readFile ./.vimrc;
+      coc = {
+        enable = true;
+        settings = {
+          "suggest.enablePreview" = true;
+          "suggest.enablePreselect" = false;
+          languageserver = {
+            clangd = {
+              command = "clangd";
+              args = ["--background-index"];
+              rootPatterns = [
+                "compile_flags.txt"
+                "compile_commands.json"
+                ".vim/"
+                ".git/"
+                ".hg/"
+              ];
+              filetypes = ["c" "cpp" "objc" "objcpp"];
+            };
+            haskell = {
+              command = "haskell-language-server-wrapper";
+              args = [ "--lsp" ];
+              rootPatterns = [
+                "*.cabal"
+                "stack.yaml"
+                "cabal.project"
+                "package.yaml"
+                "hie.yaml"
+              ];
+              filetypes = [ "hs" "lhs" "haskell" ];
+            };
+          };
+        };
+      };
     };
   };
 }
