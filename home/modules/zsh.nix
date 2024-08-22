ce1fdba741e9a828fe974ffd3847f433dc599563
nikelear <nike.lear@gmail.com>
2024-08-22 14:10:07 +0900

    add zsh-option

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -36,6 +36,10 @@
       source ${sh/prompt.sh}
       source ${sh/function.sh}
       source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+      setopt print_eight_bit
+      setopt print_exit_value
+      setopt extended_glob
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
     '';
 
     shellAliases = {
